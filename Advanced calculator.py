984d373f136ab7d3e4ce34eec10b13db2e78beeb
MrSheepCode <140061745+MrSheepCode@users.noreply.github.com>
2023-09-25 18:06:27 +0400

    Add files via upload

---
+++ Advanced calculator.py
@@ -0,0 +1,22 @@
+# Building a better calculator
+# Hope you like it haha
+
+number1 = float(input("Enter first number: "))
+operator = input("Enter operator: ")
+number2 = float(input("Enter second number: "))
+
+
+if operator == "+":
+    print(number1 + number2)
+elif operator == "-":
+    print(number1 - number2)
+elif operator == "/":
+    print(number1 / number2)
+elif operator == "*":
+    print(number1 * number2)
+else:
+    print("Invalid operation")
+
+
+
+
